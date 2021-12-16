27327548c79f1fae10cd0f9b8bdab6ef90126b38
hy221 <u.cicada.221@gmail.com>
2021-12-16 14:39:04 +0900

    add

--- MainActivity.java
+++ MainActivity.java
@@ -1,24 +1,34 @@
 package com.applicationcommunity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Objects;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
     private ToDoListDB helper = null;
-    private ArrayAdapter<String> adapter;
-
+    private int position = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,33 +44,117 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
-
-
-
     }
-
 
     @Override
     protected void onStart(){
         super.onStart();
 
-        ListView listview = findViewById(R.id.listview);
-        ArrayList<String> titleList = new ArrayList<>();
-        //ListViewに値をセットするためのアダプター
-        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,titleList);
-        listview.setAdapter(adapter);
-
-        String [] cols = {"title"};
+        ListView listView = findViewById(R.id.listview);
+        String [] title_cols = {"title","date","detail","now"};
         helper = new ToDoListDB(this);
 
-        try(SQLiteDatabase db = helper.getReadableDatabase()){ 
-            Cursor cs = db.query("todolist",cols,null,null,null,null,null,null);
-            boolean bo = cs.moveToFirst();
-            while(bo){
-                adapter.add(cs.getString(0));
-                adapter.notifyDataSetChanged();
-                bo = cs.moveToNext();
+        //コールバックメソッドOnStart内に記述する？？？
+
+        ArrayList<ListItem> data = new ArrayList<>();
+
+        //DBに登録されている値(列指定)を全て取得し、ListViewに表示させる
+        try(SQLiteDatabase db = helper.getReadableDatabase()){
+            Cursor cs = db.query("todolist",title_cols,null,null,null,null,null,null);
+            boolean boo = cs.moveToFirst();
+            while(boo){
+                //dbから取得した値をListItemオブジェクトに詰め替え
+                ListItem item = new ListItem();
+                item.setId((new Random()).nextLong());
+                item.setTitle(cs.getString(0));
+                item.setDate(cs.getString(1));
+                item.setDetail(cs.getString(2));
+                item.setKey(cs.getString(3));
+                data.add(item);
+                boo = cs.moveToNext();
+
             }
         }
+        //ListViewに値をセットするためのアダプター
+        ListAdapter adapter = new ListAdapter(this,data,R.layout.list_items);
+        listView.setAdapter(adapter);
+
+        //リストをタップしたときの処理
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //タップされた値を取得し、インテントにてToDoListDBクラスに送る
+                Intent intent = new Intent(getApplicationContext(),com.applicationcommunity.ToDoListDetail.class);
+                intent.putExtra("key",adapter.getItemKey(position));
+                startActivity(intent);
+
+                //テスト用
+                //String test = adapter.getItemKey(position);
+                //Toast toast = Toast.makeText(getApplicationContext(),),Toast.LENGTH_SHORT);
+                //toast.show();
+            }
+        });
+
+
+        //リスト長押し処理
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+
+                setPos(position);
+                alertCheck(adapter.getItemKey(position),data,adapter);
+                Toast toast = Toast.makeText(getApplicationContext(),adapter.getItemKey(position),Toast.LENGTH_SHORT);
+                toast.show();
+
+               /* String [] key = {adapter.getItemKey(position)};
+                try(SQLiteDatabase db = helper.getWritableDatabase()){
+                    //dbから値を削除
+                    db.delete("todolist","now=?",key);
+                    //ArrayListからも削除する
+                    data.remove(adapter.getItem(position));
+                    //ListView更新
+                    adapter.notifyDataSetChanged();
+                    Toast toast = Toast.makeText(getApplicationContext(),"削除しました",Toast.LENGTH_SHORT);
+                    toast.show();
+                }*/
+                //リストタップを検知させるにはfalseを指定。今回は長押しのみ検知させたいためtrueにしてる
+                return true;
+            }
+        });
+
     }
+
+    private void alertCheck(String keyval,ArrayList<ListItem> data,ListAdapter la){
+        AlertDialog.Builder alert = new AlertDialog.Builder(this);
+        alert.setTitle("削除しますか？");
+        alert.setPositiveButton("はい", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String [] key = {keyval};
+                try(SQLiteDatabase db = helper.getReadableDatabase()){
+                    db.delete("todolist","now=?",key);
+                    data.remove(la.getItem(getPos()));
+                    la.notifyDataSetChanged();
+                }
+                Toast toast = Toast.makeText(getApplicationContext(),"削除しました",Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+        alert.setNeutralButton("キャンセル", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+            }
+        });
+        alert.show();
+    }
+
+    private  void setPos(int pos){
+        position = pos;
+    }
+
+    private int getPos(){
+        return position;
+    }
+
+
 }
--- ToDoListDetail.java
+++ ToDoListDetail.java
@@ -1,6 +1,8 @@
 package com.applicationcommunity;
 
 import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
@@ -8,6 +10,7 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
@@ -17,6 +20,7 @@ import java.util.Calendar;
 public class ToDoListDetail extends AppCompatActivity {
 
     private ToDoListDB helper = null;
+    private String keyvalue = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,28 +42,86 @@ public class ToDoListDetail extends AppCompatActivity {
         button_done.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // 今日日付を取得
-                final CharSequence nowdate = android.text.format.DateFormat.format("yyyy/MM/dd kk:mm:ss", Calendar.getInstance());
 
                 //値を取得
-                String now = nowdate.toString();                         //今日の日付
                 String title = editText_title.getText().toString();     //タイトル
                 String date = textview_cal.getText().toString();        //日付
                 String time = textview_time.getText().toString();       //時刻
                 String detail = textview_detail.getText().toString();   //詳細
 
-                try (SQLiteDatabase db = helper.getWritableDatabase()){
-                    ContentValues cv = new ContentValues();
-                    cv.put("now",now);
-                    cv.put("title",title);
-                    cv.put("date",date);
-                    cv.put("time",time);
-                    cv.put("detail",detail);
-                    db.insert("todolist",null,cv);
+                //新規作成
+                if(keyvalue == null) {
+                    // 今日日付を取得
+                    final CharSequence nowdate = android.text.format.DateFormat.format("yyyy/MM/dd kk:mm:ss", Calendar.getInstance()); //今日の日付
+                    keyvalue = nowdate.toString();
+                    //DB登録
+                    try (SQLiteDatabase db = helper.getWritableDatabase()){
+                        ContentValues cv = new ContentValues();
+                        cv.put("now",keyvalue);
+                        cv.put("title",title);
+                        cv.put("date",date);
+                        cv.put("time",time);
+                        cv.put("detail",detail);
+                        //db.insertWithOnConflict("todolist",null,cv,SQLiteDatabase.CONFLICT_REPLACE);
+                        db.insert("todolist",null,cv);
+                    }
+                    //DB更新
+                }else{
+                        String [] Key = {keyvalue};
+                    try (SQLiteDatabase db = helper.getWritableDatabase()){
+                        ContentValues cv = new ContentValues();
+                        cv.put("now",keyvalue);
+                        cv.put("title",title);
+                        cv.put("date",date);
+                        cv.put("time",time);
+                        cv.put("detail",detail);
+                        db.update("todolist",cv,"now=?",Key);
+                    }
                 }
                 finish();
             }
         });
+
+        Intent intent = getIntent();
+        keyvalue = intent.getStringExtra("key");
+        //メインアクティビティから受け取ったインテントに値があれば以下が実行される タップされたリストのkeyと一致する行にカーソルを移動させたのち、DBから値を取得。その後各欄に値を代入
+        if(!(keyvalue == null)){
+            String [] cols = {"title","date","detail","time","now"};
+            String [] key = {intent.getStringExtra("key")};
+            try(SQLiteDatabase db = helper.getReadableDatabase()){
+                Cursor cs = db.query("todolist",cols,"now=?",key,null,null,null,null);
+
+                if(cs.moveToFirst()){
+                    editText_title.setText(cs.getString(0));
+                    textview_cal.setText(cs.getString(1));
+                    textview_detail.setText(cs.getString(2));
+                    textview_time.setText(cs.getString(3));
+                }else{
+                    Toast toast = Toast.makeText(getApplicationContext(),"nulllllll",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        }
+
+       /* Intent intent = getIntent();
+        //メインアクティビティから受け取ったインテントに値があれば以下が実行される タップされたリストポジションID行目にカーソルを移動させたのち、DBから値を取得。その後タイトル欄に値を代入
+        //※リストを並べ替えるとこの部分は機能しなくなるので要検討
+        if(!(-1==intent.getIntExtra("keyid",-1))){  //defaultvalueはとりあえず-1にしとく
+            String [] cols = {"title"};
+            try(SQLiteDatabase db = helper.getReadableDatabase()){
+                Cursor cs = db.query("todolist",cols,null,null,null,null,null,null);
+
+                if(cs.moveToFirst()){
+                    cs.moveToPosition(intent.getIntExtra("keyid",-1));
+                    editText_title.setText(cs.getString(0));
+                }else{
+                    Toast toast = Toast.makeText(getApplicationContext(),"nulllllll",Toast.LENGTH_SHORT);
+                    toast.show();
+                }
+            }
+        }*/
+
+
         //カレンダーボタンクリック処理
         button_cal.setOnClickListener(new View.OnClickListener() {
             @Override
